5c05820c580dd3c4a3f9466221262a60e4d37497
Emir <emirbek.nurbekov@alatoo.edu.kg>
2022-01-18 18:56:29 +0600

    api

--- src/config/const.js
+++ src/config/const.js
@@ -1,3 +1,3 @@
-// export const API = 'https://hi-doctor-emirbek-aktilek.herokuapp.com/api'
+export const API = 'https://hi-doctor-emirbek-aktilek.herokuapp.com/api'
 
-export const API = 'http://localhost:8000'
+// export const API = 'http://localhost:8000'
